529b380514d70943c74aaa9ba9d6c47a85b25f49
Breno Hasparyk <breno.ha@gmail.com>
2021-03-04 17:01:04 -0300

    Add chronometer with useState and useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
 import ViewComponent from '@app/components/View';
+import { IUser } from '@app/interfaces/user';
 
-const HomePage = () => {
-  return <ViewComponent />;
+type Props = {
+  user: IUser;
 };
+
+const HomePage = ({ user }: Props) => {
+  return (
+    <div>
+      <ViewComponent name={user.name} />;
+    </div>
+  );
+};
+
+export async function getStaticProps(context) {
+  const res = await fetch(`https://api.github.com/users/BrenoHA`);
+  const user: IUser = await res.json();
+
+  if (!user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { user },
+  };
+}
 
 export default HomePage;
