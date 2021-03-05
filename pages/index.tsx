1349a1b0b0b14546a4f31285e0e7b1849c34fd60
Breno Hasparyk <breno.ha@gmail.com>
2021-03-05 17:42:35 -0300

    Add GitHub Input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,11 @@
 import ViewComponent from '@app/components/View';
-import { IUser } from '@app/interfaces/user';
 
-type Props = {
-  user: IUser;
-};
-
-const HomePage = ({ user }: Props) => {
+const HomePage = () => {
   return (
     <div>
-      <ViewComponent name={user.name} />;
+      <ViewComponent />;
     </div>
   );
 };
-
-export async function getStaticProps(context) {
-  const res = await fetch(`https://api.github.com/users/BrenoHA`);
-  const user: IUser = await res.json();
-
-  if (!user) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: { user },
-  };
-}
 
 export default HomePage;
