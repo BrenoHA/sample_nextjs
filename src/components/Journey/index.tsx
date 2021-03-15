3baaaa78aab724e022290f3799efac41d41f0f25
Breno Hasparyk <breno.ha@gmail.com>
2021-03-15 14:27:06 -0300

    Add Modal as a component

--- src/components/Journey/index.tsx
+++ src/components/Journey/index.tsx
@@ -3,48 +3,23 @@ import { Button, Modal, Row, Col, Form, Card } from 'react-bootstrap';
 import styles from './styles.module.scss';
 import { JourneyStoreClass } from '@app/stores/JourneyStore';
 import { observer } from 'mobx-react';
+import ModalJourneyComponent from './Modal';
 
 type JourneyListProps = {
   journeyStore: JourneyStoreClass;
 };
 
 const JourneyComponent = observer(({ journeyStore }: JourneyListProps) => {
-  const [type, setType] = useState('');
-  const [startTime, setStartTime] = useState('');
-  const [endTime, setEndTime] = useState('');
-  const [showModal, setShowModal] = useState(false);
-
+  const [showModal, setShowModal] = useState<boolean>(false);
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
-
   const status = journeyStore.status;
-
-  const handleSubmit = (ev: SyntheticEvent) => {
-    ev.preventDefault();
-    if (startTime) {
-      journeyStore.addJourney(type, startTime, endTime);
-      setStartTime('');
-    }
-  };
-
-  const handleStartTimeChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    ev.preventDefault();
-    setStartTime(ev.target.value);
-  };
-  const handleTypeChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    ev.preventDefault();
-    setType(ev.target.value);
-  };
-  const handleEndTimeChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    ev.preventDefault();
-    setEndTime(ev.target.value);
-  };
 
   return (
     <>
       <div className={styles.center}>
         <div className={styles.container}>
-          <span>Adicione uma jornada</span>
+          <span>Adicione um Evento</span>
           <Button
             onClick={handleShow}
             variant="outline-primary"
@@ -52,11 +27,15 @@ const JourneyComponent = observer(({ journeyStore }: JourneyListProps) => {
           >
             Adicionar
           </Button>
-
+          <Modal size="lg" show={showModal} onHide={handleClose} centered>
+            <ModalJourneyComponent
+              journeyStore={journeyStore}
+              setShowModal={setShowModal}
+            />
+          </Modal>
           <span>Feitos: {status.completed} </span>
           <span> Remanecentes: {status.remaining}</span>
         </div>
-
         <div className="d-flex justify-content-start mt-4 ">
           <div>
             {journeyStore.journeys.map((journey, index) => {
@@ -67,7 +46,13 @@ const JourneyComponent = observer(({ journeyStore }: JourneyListProps) => {
                   style={{ width: '18rem' }}
                   key={index}
                 >
-                  <Card.Header>{journey.type}</Card.Header>
+                  <Card.Header
+                    onClick={() => {
+                      journeyStore.setCompleted(journey.id);
+                    }}
+                  >
+                    [ {journey.completed ? 'x' : '\xa0\xa0'} ] {journey.type}
+                  </Card.Header>
                   <Card.Body>
                     <Card.Text>
                       Início: {journey.startTime}
@@ -81,61 +66,7 @@ const JourneyComponent = observer(({ journeyStore }: JourneyListProps) => {
           </div>
         </div>
       </div>
-
-      <Modal size="lg" show={showModal} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Nova Jornada</Modal.Title>
-        </Modal.Header>
-        <Form onSubmit={handleSubmit}>
-          <Modal.Body>
-            <Row>
-              <Col>
-                <Form.Label>Tipo</Form.Label>
-                <Form.Control
-                  as="select"
-                  aria-label="tipo"
-                  onChange={handleTypeChange}
-                >
-                  <option>-</option>
-                  <option>Descanso</option>
-                  <option>Espera</option>
-                  <option>Refeição</option>
-                  <option>Pernoite</option>
-                </Form.Control>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Label>Início</Form.Label>
-                <Form.Control
-                  type="datetime-local"
-                  aria-label="StartTime"
-                  value={startTime}
-                  onChange={handleStartTimeChange}
-                />
-              </Col>
-              <Col>
-                <Form.Label>Fim</Form.Label>
-                <Form.Control
-                  type="datetime-local"
-                  aria-label="EndTime"
-                  onChange={handleEndTimeChange}
-                />
-              </Col>
-            </Row>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Fechar
-            </Button>
-            <Button variant="primary" type="submit" onClick={handleClose}>
-              Salvar Jornada
-            </Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
     </>
   );
 });
-
 export default JourneyComponent;
