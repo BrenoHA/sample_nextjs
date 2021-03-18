ea43cffb90cf900ff73288bafafb846cbdbc2e93
BrenoHA <breno.ha@gmail.com>
2021-03-18 16:23:38 -0300

    Add Footer and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 
 import NavBarComponent from '@app/components/NavBar';
+import FooterComponent from '@app/components/Footer';
 
 import '@app/styles/bootstrap.theme.scss';
 import '@app/styles/global.css';
@@ -26,6 +27,7 @@ function MyApp({ Component, pageProps }) {
       >
         <Component {...pageProps} />
       </motion.div>
+      <FooterComponent />
     </>
   );
 }
