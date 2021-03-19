18416c1e1a07ae04cabb638b911a315063462c40
BrenoHA <breno.ha@gmail.com>
2021-03-19 16:31:17 -0300

    Add Chronometer Page

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,8 +4,9 @@ import Link from 'next/link';
 const NavBarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="md" fixed="top">
-      <Navbar.Brand href="/">React Next Aplication</Navbar.Brand>
-
+      <Link href="/" passHref>
+        <Navbar.Brand href="/">React Next Aplication</Navbar.Brand>
+      </Link>
       <Navbar.Toggle />
 
       <Navbar.Collapse id="responsive-navbar-nav">
@@ -13,6 +14,9 @@ const NavBarComponent = () => {
         <Nav className="mr-4">
           <Link href="/" passHref>
             <Nav.Link className="mr-2">Início</Nav.Link>
+          </Link>
+          <Link href="/chronometer" passHref>
+            <Nav.Link className="mr-2">Cronometro</Nav.Link>
           </Link>
           <Link href="/journey" passHref>
             <Nav.Link>Jornada</Nav.Link>
